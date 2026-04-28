aa786c343a058b83d0c01f0b599f84524dc48e10
Antoni Roman <antonipaulo.roman@gmail.com>
2026-04-27 20:51:37 -0500

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { GameProvider, useGame } from "./GameContext";
+import WelcomeScreen from "./WelcomeScreen";
+import GameBoard from "./GameBoard";
+import "./index.css";
+
+function ScreenRouter() {
+  const { screen } = useGame();
+
+  return screen === "welcome" ? <WelcomeScreen /> : <GameBoard />;
+}
+
 export default function App() {
-  return <></>;
+  return (
+    <GameProvider>
+      <ScreenRouter />
+    </GameProvider>
+  );
 }
